3d482699750b104e57d75b468456188da2df7505
Marcel Walter <marcel.walter@tum.de>
2024-11-06 20:12:09 +0100

    :bookmark: Bump version to 0.0.10

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ with open(README_PATH, encoding="utf8") as readme_file:
 
 setup(
     name='aigverse',
-    version='0.0.9',
+    version='0.0.10',
     author='Marcel Walter',
     author_email='marcel.walter@tum.de',
     description='A Python library for working with logic networks, synthesis, and optimization.',
