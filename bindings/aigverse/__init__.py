4c5d6070cfbe644a8052dfc2c8da371e0240a3db
Marcel Walter <marcel.walter@tum.de>
2024-11-07 19:41:50 +0100

    :snake: Added contents of the library to `__init__.py` for proper importing

--- bindings/aigverse/__init__.py
+++
@@ -1,5 +0,0 @@
-from .aigverse import (AigNode, AigSignal, Aig, DepthAig, AigEdgeList, AigNode, AigEdgeList, TruthTable,
-                       equivalence_checking, sop_refactoring, aig_resubstitution, aig_cut_rewriting, simulate)
-
-__all__ = ['Aig', 'DepthAig', 'AigNode', 'AigSignal', 'AigEdgeList', 'AigNode', 'AigEdgeList', 'TruthTable',
-           'equivalence_checking', 'sop_refactoring', 'aig_resubstitution', 'aig_cut_rewriting', 'simulate']
