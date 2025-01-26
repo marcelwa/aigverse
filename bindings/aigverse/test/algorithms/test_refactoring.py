56418a9bb791aab147f984de593d8fa0d5b07cdb
Marcel Walter <marcel.walter@tum.de>
2025-01-26 20:10:21 +0100

    :test_tube: Attempt to test which instance causes SEGFAULTs

--- bindings/aigverse/test/algorithms/test_refactoring.py
+++ bindings/aigverse/test/algorithms/test_refactoring.py
@@ -1,84 +1,82 @@
 from aigverse import Aig, equivalence_checking, sop_refactoring
 
-
-def test_empty_aigs() -> None:
-    aig1 = Aig()
-    aig2 = aig1.clone()
-
-    sop_refactoring(aig1)
-
-    assert equivalence_checking(aig1, aig2)
-
-
-def test_simple_aigs() -> None:
-    aig1 = Aig()
-    aig2 = Aig()
-
-    a1 = aig1.create_pi()
-    b1 = aig1.create_pi()
-
-    and1 = aig1.create_and(a1, b1)
-
-    aig1.create_po(and1)
-    aig1.create_po(b1)
-
-    a2 = aig2.create_pi()
-    b2 = aig2.create_pi()
-
-    and2 = aig2.create_and(a2, b2)
-
-    aig2.create_po(and2)
-    aig2.create_po(b2)
-
-    sop_refactoring(aig1)
-
-    assert equivalence_checking(aig1, aig2)
-    assert equivalence_checking(aig1, aig1.clone())
-
-
-def test_aig_and_its_negated_copy() -> None:
-    aig1 = Aig()
-
-    a1 = aig1.create_pi()
-    b1 = aig1.create_pi()
-    c1 = aig1.create_pi()
-
-    and1 = aig1.create_and(a1, b1)
-    and2 = aig1.create_and(~a1, c1)
-    and3 = aig1.create_and(and1, and2)
-
-    aig2 = aig1.clone()
-
-    aig1.create_po(and3)
-
-    aig2.create_po(~and3)
-
-    sop_refactoring(aig1)
-
-    assert not equivalence_checking(aig1, aig2)
-
-    sop_refactoring(aig2)
-
-    assert not equivalence_checking(aig1, aig2)
-
-
-#
-# def test_equivalent_node_merger() -> None:
-#     # x0 * !(!x0 * !x1) == > x0 (reduction of 2 nodes)
+# def test_empty_aigs() -> None:
 #     aig1 = Aig()
-#     x0 = aig1.create_pi()
-#     x1 = aig1.create_pi()
-#     n0 = aig1.create_and(~x0, ~x1)
-#     n1 = aig1.create_and(x0, ~n0)
-#     aig1.create_po(n1)
-#
-#     aig_before = aig1.clone()
+#     aig2 = aig1.clone()
 #
 #     sop_refactoring(aig1)
 #
-#     assert aig1.size() == aig_before.size() - 2
+#     assert equivalence_checking(aig1, aig2)
 #
-#     assert equivalence_checking(aig1, aig_before)
+#
+# def test_simple_aigs() -> None:
+#     aig1 = Aig()
+#     aig2 = Aig()
+#
+#     a1 = aig1.create_pi()
+#     b1 = aig1.create_pi()
+#
+#     and1 = aig1.create_and(a1, b1)
+#
+#     aig1.create_po(and1)
+#     aig1.create_po(b1)
+#
+#     a2 = aig2.create_pi()
+#     b2 = aig2.create_pi()
+#
+#     and2 = aig2.create_and(a2, b2)
+#
+#     aig2.create_po(and2)
+#     aig2.create_po(b2)
+#
+#     sop_refactoring(aig1)
+#
+#     assert equivalence_checking(aig1, aig2)
+#     assert equivalence_checking(aig1, aig1.clone())
+#
+#
+# def test_aig_and_its_negated_copy() -> None:
+#     aig1 = Aig()
+#
+#     a1 = aig1.create_pi()
+#     b1 = aig1.create_pi()
+#     c1 = aig1.create_pi()
+#
+#     and1 = aig1.create_and(a1, b1)
+#     and2 = aig1.create_and(~a1, c1)
+#     and3 = aig1.create_and(and1, and2)
+#
+#     aig2 = aig1.clone()
+#
+#     aig1.create_po(and3)
+#
+#     aig2.create_po(~and3)
+#
+#     sop_refactoring(aig1)
+#
+#     assert not equivalence_checking(aig1, aig2)
+#
+#     sop_refactoring(aig2)
+#
+#     assert not equivalence_checking(aig1, aig2)
+
+
+def test_equivalent_node_merger() -> None:
+    # x0 * !(!x0 * !x1) == > x0 (reduction of 2 nodes)
+    aig1 = Aig()
+    x0 = aig1.create_pi()
+    x1 = aig1.create_pi()
+    n0 = aig1.create_and(~x0, ~x1)
+    n1 = aig1.create_and(x0, ~n0)
+    aig1.create_po(n1)
+
+    aig_before = aig1.clone()
+
+    sop_refactoring(aig1)
+
+    assert aig1.size() == aig_before.size() - 2
+
+    assert equivalence_checking(aig1, aig_before)
 
 
 # def test_positive_divisor_substitution() -> None:
