97037e19e092f8b9f34381ad2f38e328233fa51e
Marcel Walter <marcel.walter@tum.de>
2025-04-10 17:42:37 +0200

    :green_heart: Fallback to ClangCL on Windows in CI environment

--- noxfile.py
+++ noxfile.py
@@ -43,6 +43,7 @@ def _run_tests(
     env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
     if os.environ.get("CI", None) and sys.platform == "win32":
         env["CMAKE_GENERATOR"] = "Ninja"
+        env["SKBUILD_CMAKE_ARGS"] = "-T ClangCL"
 
     if shutil.which("cmake") is None and shutil.which("cmake3") is None:
         session.install("cmake")
