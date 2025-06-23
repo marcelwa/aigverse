d4b82e2c756532fdbe0a20f1f83640728ddfb2de
Marcel Walter <marcel.walter@tum.de>
2025-06-23 13:54:39 +0200

    :bug: Update fanout computation in `to_networkx` function to use `fanout_size` method

--- bindings/aigverse/test/networks/test_fanout_aig.py
+++ bindings/aigverse/test/networks/test_fanout_aig.py
@@ -16,14 +16,16 @@ def test_fanout_aig() -> None:
     n4 = aig.create_and(x1, x2)
     n5 = aig.create_and(n4, x3)
     n6 = aig.create_and(n4, n5)
+
     # Create primary outputs
     aig.create_po(n6)
+
     # Check the fanout of n4
     fanout_list = aig.fanouts(aig.get_node(n4))
     assert len(fanout_list) == 2
-    assert ((fanout_list[0] == aig.get_node(n6)) and (fanout_list[1] == aig.get_node(n5))) or (
-        (fanout_list[0] == aig.get_node(n5)) and (fanout_list[1] == aig.get_node(n6))
-    )
-    # fanouts() size only collect internal fanouts(No PO) while fanout_size() will collect all
+    assert aig.get_node(n6) in fanout_list
+    assert aig.get_node(n5) in fanout_list
+
+    # fanouts() only collect internal fanouts (no POs) while fanout_size() will collect all
     assert len(aig.fanouts(aig.get_node(n6))) == 0
     assert aig.fanout_size(aig.get_node(n6)) == 1
