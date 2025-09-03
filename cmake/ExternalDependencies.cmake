1e0c140bbe5fda18fd73e30b6483a403591a6e10
Marcel Walter <marcel.walter@tum.de>
2025-09-03 13:07:41 +0200

    :art: CodeRabbit suggestion

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -10,8 +10,16 @@ if(NOT SKBUILD)
   execute_process(
     COMMAND "${Python_EXECUTABLE}" -m pybind11 --cmakedir
     OUTPUT_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE pybind11_DIR)
-  list(APPEND CMAKE_PREFIX_PATH "${pybind11_DIR}")
+    OUTPUT_VARIABLE pybind11_DIR
+    RESULT_VARIABLE pybind11_exec_res
+    ERROR_VARIABLE pybind11_exec_err
+  )
+  if(pybind11_exec_res EQUAL 0 AND pybind11_DIR)
+    list(APPEND CMAKE_PREFIX_PATH "${pybind11_DIR}")
+  else()
+    # Don't modify CMAKE_PREFIX_PATH on failure; helpful debug output
+    message(DEBUG "pybind11 --cmakedir query failed (code=${pybind11_exec_res}). stderr: ${pybind11_exec_err}")
+  endif()
 endif()
 
 message(STATUS "Python executable: ${Python_EXECUTABLE}")
