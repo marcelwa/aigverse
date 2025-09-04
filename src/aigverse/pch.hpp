2e1c29424760f5fd904add9f2267c2a7633fe98c
Marcel Walter <marcel.walter@tum.de>
2025-09-04 10:47:42 +0200

    :recycle: Clean up pre-compiled headers

--- src/aigverse/pch.hpp
+++ src/aigverse/pch.hpp
@@ -14,8 +14,8 @@
 #include <fmt/format.h>
 
 // mockturtle frequently used minimal set
-#include <mockturtle/algorithms/cleanup.hpp>
 #include <mockturtle/networks/aig.hpp>
+#include <mockturtle/networks/sequential.hpp>
 #include <mockturtle/traits.hpp>
 #include <mockturtle/views/depth_view.hpp>
 #include <mockturtle/views/fanout_view.hpp>
@@ -24,24 +24,17 @@
 #include <kitty/dynamic_truth_table.hpp>
 
 // STL basics
+#include <array>
 #include <cstdint>
 #include <functional>
+#include <iostream>
 #include <map>
+#include <optional>
 #include <set>
+#include <stdexcept>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
-
-// lorina parsers
-#include <lorina/aiger.hpp>
-#include <lorina/diagnostics.hpp>
-#include <lorina/verilog.hpp>
-
-// mockturtle IO writers/readers
-#include <mockturtle/io/aiger_reader.hpp>
-#include <mockturtle/io/verilog_reader.hpp>
-#include <mockturtle/io/write_aiger.hpp>
-#include <mockturtle/io/write_dot.hpp>
-#include <mockturtle/io/write_verilog.hpp>
