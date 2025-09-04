5d008af3210d399c793f0ff819131539afd7eedd
Marcel Walter <marcel.walter@tum.de>
2025-09-04 13:25:54 +0200

    :goal_net: Add range check for variable index in cofactor functions

--- src/aigverse/truth_tables/operations.cpp
+++ src/aigverse/truth_tables/operations.cpp
@@ -9,6 +9,8 @@
 #include <kitty/operations.hpp>
 #include <pybind11/pybind11.h>
 
+#include <stdexcept>
+
 namespace aigverse
 {
 
@@ -26,14 +28,30 @@ static void bind_truth_table_operations(pybind11::module_& m)
         "Compute the ternary majority of three truth tables.", pybind11::call_guard<pybind11::gil_scoped_release>());
 
     m.def(
-        "cofactor0", [](const aigverse::truth_table& tt, const uint8_t var_index)
-        { return kitty::cofactor0(tt, var_index); }, "tt"_a, "var_index"_a,
+        "cofactor0",
+        [](const aigverse::truth_table& tt, const uint8_t var_index)
+        {
+            if (var_index >= tt.num_vars())
+            {
+                throw std::invalid_argument("var_index out of range");
+            }
+            return kitty::cofactor0(tt, var_index);
+        },
+        "tt"_a, "var_index"_a,
         "Returns the cofactor with respect to 0 of the variable at index `var_index` in the given truth table.",
         pybind11::call_guard<pybind11::gil_scoped_release>());
 
     m.def(
-        "cofactor1", [](const aigverse::truth_table& tt, const uint8_t var_index)
-        { return kitty::cofactor1(tt, var_index); }, "tt"_a, "var_index"_a,
+        "cofactor1",
+        [](const aigverse::truth_table& tt, const uint8_t var_index)
+        {
+            if (var_index >= tt.num_vars())
+            {
+                throw std::invalid_argument("var_index out of range");
+            }
+            return kitty::cofactor1(tt, var_index);
+        },
+        "tt"_a, "var_index"_a,
         "Returns the cofactor with respect to 1 of the variable at index `var_index` in the given truth table.",
         pybind11::call_guard<pybind11::gil_scoped_release>());
 }
